fd8e17ff1b7a871738b8660cafc0b4c2ca730078
unknown <Jegorovsv@gmail.com>
2020-04-29 22:51:25 +0300

    forgot li's exist, started work on expression builder

--- main.js
+++ main.js
@@ -19,4 +19,38 @@ function run() {
   let inputArray = [...document.querySelectorAll("input")].map((x) => x.value);
   inputArray = inputArray.slice(0, -1);
   console.log(inputArray);
+  expressionBuilder(inputArray);
 }
+
+// function to display analyzed expression
+function expressionBuilder(inputArgs) {
+  const baseExpression = "f(x, y) = x − ay + bx^2 + cxy + dy^2";
+  let finalExpression = inputArgs
+    ? `f(x, y) = ${finalExpressionBuilder()}`
+    : baseExpression;
+  console.log(finalExpression);
+  let expressionOutput = document.querySelector(".expression");
+  console.log(expressionOutput, "This is the div with output");
+
+  [...expressionOutput.childNodes].forEach((x) => {
+    console.log("loggin children?");
+    console.log(x);
+    x.parentNode.removeChild(x);
+  });
+
+  expressionOutput.appendChild(document.createTextNode(finalExpression));
+
+  function finalExpressionBuilder(inputs) {
+    let finalExpressionConcatenated = "haha";
+
+    return finalExpressionConcatenated;
+  }
+}
+
+function intiializeApp() {
+  expressionBuilder();
+  // bind submit button to run everything
+  document.querySelector(".submitBtn").addEventListener("click", run);
+}
+
+intiializeApp();
