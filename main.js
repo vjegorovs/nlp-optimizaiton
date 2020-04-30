d3308bf5d37962fb017b01a5b7cd91995ea9d698
unknown <Jegorovsv@gmail.com>
2020-04-30 22:54:07 +0300

    finished expressionbuilder for output, also let it cast global calculation settings to a global settings object

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let Fraction = algebra.Fraction;
 let Expression = algebra.Expression;
 let Equation = algebra.Equation;
+const globalSettings = { xStart: null, yStart: null, eps: null, t: null };
 
 let expr = new Expression("x");
 expr = expr.multiply("x").subtract(3).add("x*y");
@@ -25,24 +26,25 @@ function run() {
 // function to display analyzed expression
 function expressionBuilder(inputArgs) {
   const baseExpression = "f(x, y) = x − ay + bx^2 + cxy + dy^2";
-  let finalExpression = inputArgs
-    ? `f(x, y) = ${finalExpressionBuilder()}`
+  const finalExpression = inputArgs
+    ? `f(x, y) = ${finalExpressionBuilder(inputArgs)}`
     : baseExpression;
   console.log(finalExpression);
-  let expressionOutput = document.querySelector(".expression");
-  console.log(expressionOutput, "This is the div with output");
+  const expressionOutput = document.querySelector(".expression");
 
-  [...expressionOutput.childNodes].forEach((x) => {
-    console.log("loggin children?");
-    console.log(x);
-    x.parentNode.removeChild(x);
-  });
+  // removes previous expr
+  [...expressionOutput.childNodes].forEach((x) => x.parentNode.removeChild(x));
 
   expressionOutput.appendChild(document.createTextNode(finalExpression));
 
   function finalExpressionBuilder(inputs) {
-    let finalExpressionConcatenated = "haha";
-
+    //I thought abough converting to floats/ints here, but I guess I will carry on all the way with Strings, since math.js / algebra.js work with Strings for derivatives etc
+    let finalExpressionConcatenated = `x − ${inputs[0]}y + ${inputs[1]}x^2 + ${inputs[2]}xy + ${inputs[3]}y^2`;
+    globalSettings.xStart = inputs[4] ? inputs[4] : 0;
+    globalSettings.yStart = inputs[5] ? inputs[5] : 0;
+    globalSettings.eps = inputs[6] ? inputs[6] : 0;
+    globalSettings.t = inputs[7] ? inputs[7] : 0;
+    console.log(globalSettings);
     return finalExpressionConcatenated;
   }
 }
